e453195a736a09a62022f3600c0feca92082b796
rumos <gyunjin3@gmail.com>
2024-05-05 22:46:32 +0900

    Spring Boot 프로젝트 생성

---
+++ dao-dto-practice/src/main/java/com/rumos/daodtopractice/DaoDtoPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.rumos.daodtopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoDtoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoDtoPracticeApplication.class, args);
+	}
+
+}
---
+++ dao-dto-practice/src/test/java/com/rumos/daodtopractice/DaoDtoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumos.daodtopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoDtoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
